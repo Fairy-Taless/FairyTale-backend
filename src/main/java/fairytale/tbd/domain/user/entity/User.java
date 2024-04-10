3c48c6b9740c230c74c5416409751571627d3976
junhaa <2171326@hansung.ac.kr>
2024-04-10 16:32:37 +0900

    :sparkles: FEAT. User entity 추가

---
+++ src/main/java/fairytale/tbd/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package fairytale.tbd.domain.user.entity;
+
+
+import fairytale.tbd.domain.user.enums.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "login_id", nullable = false)
+	private String loginId;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "gender", nullable = false)
+	private Gender gender;
+
+	@Column(name = "username", nullable = false)
+	private String username;
+
+}
---
+++ src/main/java/fairytale/tbd/domain/user/enums/Gender.java
@@ -0,0 +1,5 @@
+package fairytale.tbd.domain.user.enums;
+
+public enum Gender {
+	MALE, FEMALE;
+}
