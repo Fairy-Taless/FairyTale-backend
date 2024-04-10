13bbdac006d217ebfca5cb6d4055f2cb996c6839
junhaa <2171326@hansung.ac.kr>
2024-04-11 01:56:34 +0900

    :sparkles: FEAT. ElevenlabsManager.java 파일 변환 코드 변경 및 PostConstruct 추가

--- src/main/java/fairytale/tbd/global/elevenlabs/ElevenlabsManager.java
+++ src/main/java/fairytale/tbd/global/elevenlabs/ElevenlabsManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +20,7 @@ import net.andrewcpu.elevenlabs.model.voice.VoiceSettings;
 
 import fairytale.tbd.global.elevenlabs.exception.FileConvertException;
 import fairytale.tbd.global.enums.statuscode.ErrorStatus;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,6 +34,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ElevenlabsManager {
 
+	private static final Logger LOGGER = LogManager.getLogger(ElevenlabsManager.class);
+
 	@Value("${voice.elevenlabs.apikey}")
 	private String apiKey;
 
@@ -43,16 +48,20 @@ public class ElevenlabsManager {
 	@Value("${voice.elevenlabs.voice_setting.use_speaker_boost}")
 	private boolean useSpeakerBoost;
 
+	@PostConstruct
+	public void init(){
+		ElevenLabs.setApiKey(apiKey);
+		ElevenLabs.setDefaultModel("eleven_multilingual_v2");
+	}
+
+
 	/**
 	 * EleventLabs TTS 변환
 	 * @param text 음성 TTS 변환 할 내용
 	 * @param voiceId voice 고유 값
 	 * @return 생성된 .mpga 파일
 	 */
-
 	public File elevenLabsTTS(String text, String voiceId){
-		ElevenLabs.setApiKey(apiKey);
-		ElevenLabs.setDefaultModel("eleven_multilingual_v2");
 		return SpeechGenerationBuilder.textToSpeech()
 			.file()
 			.setText(text)
@@ -70,16 +79,17 @@ public class ElevenlabsManager {
 	 * @return voice 고유번호
 	 */
 	public String addVoice(String userName, MultipartFile sample) {
+		File file = convertMultipartFileToFile(sample);
 		VoiceBuilder builder = new VoiceBuilder();
 		builder.withName(userName);
-		builder.withFile(convertMultipartFileToFile(sample));
+		builder.withFile(file);
+		builder.withDescription("the emotional voice of the main character of a children's book");
 		builder.withLabel("language", "ko");
 		Voice voice = builder.create();
 		return voice.getVoiceId();
 	}
 
 
-	// TODO IOException 예외 처리 => 500
 
 	/**
 	 * MultiPartFile -> File 변환
@@ -91,13 +101,8 @@ public class ElevenlabsManager {
 		File file = new File(System.getProperty("java.io.tmpdir") + "/" + multipartFile.getOriginalFilename());
 
 		// MultipartFile의 내용을 파일에 쓰기
-		try (InputStream inputStream = multipartFile.getInputStream();
-			 FileOutputStream outputStream = new FileOutputStream(file)) {
-			int read;
-			byte[] bytes = new byte[1024];
-			while ((read = inputStream.read(bytes)) != -1) {
-				outputStream.write(bytes, 0, read);
-			}
+		try {
+			multipartFile.transferTo(file);
 		}
 		catch (Exception e){
 			e.printStackTrace();
