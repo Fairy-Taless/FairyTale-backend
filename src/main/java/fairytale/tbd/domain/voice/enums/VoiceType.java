a58b3c1335664e9b894c68ad4fc9e8fe4883b7d2
junhaa <2171326@hansung.ac.kr>
2024-05-02 08:52:29 +0900

    :sparkles: FEAT. VoiceType.java 문장 음성 종류 추가

--- src/main/java/fairytale/tbd/domain/voice/enums/VoiceType.java
+++ src/main/java/fairytale/tbd/domain/voice/enums/VoiceType.java
@@ -1,5 +1,6 @@
 package fairytale.tbd.domain.voice.enums;
 
 public enum VoiceType {
-	OLD_WOMAN, OLD_MAN, YOUNG_WOMAN, YOUNG_MAN;
+	OLD_WOMAN, OLD_MAN, YOUNG_WOMAN, YOUNG_MAN, NARRATION, MIDDLEAGE_MAN, MIDDLEAGE_WOMAN;
 }
+
