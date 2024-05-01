c68764339d9c41345d0e98af5796092fa15be988
ghvfgfcht <kjh03124@naver.com>
2024-05-01 18:01:42 +0900

    :sparkles: FEAT. FaceSwapRestController 추가

---
+++ src/main/java/fairytale/tbd/domain/faceSwap/web/controller/FaceSwapRestController.java
@@ -0,0 +1,27 @@
+package fairytale.tbd.domain.faceSwap.web.controller;
+
+import fairytale.tbd.domain.faceSwap.service.PhotoUploadServiceImpl;
+import fairytale.tbd.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/fjaceSwap")
+@RequiredArgsConstructor
+public class FaceSwapRestController {
+
+    private final PhotoUploadServiceImpl photoUploadService;
+
+    @PostMapping("/uploadImg")
+    public void uploadImg(@RequestParam MultipartFile file) throws IOException{
+        photoUploadService.savePhotos(file);
+    }
+}
