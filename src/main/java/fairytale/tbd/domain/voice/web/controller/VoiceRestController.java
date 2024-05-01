f5adfd54460eb956658726792687b7c8f028e7f0
junhaa <2171326@hansung.ac.kr>
2024-05-01 20:51:54 +0900

    :sparkles: FEAT. Voice에서 User 연관 관계 추가

--- src/main/java/fairytale/tbd/domain/voice/web/controller/VoiceRestController.java
+++ src/main/java/fairytale/tbd/domain/voice/web/controller/VoiceRestController.java
@@ -4,15 +4,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fairytale.tbd.domain.user.entity.User;
 import fairytale.tbd.domain.voice.converter.VoiceConverter;
 import fairytale.tbd.domain.voice.entity.Voice;
 import fairytale.tbd.domain.voice.service.VoiceCommandService;
 import fairytale.tbd.domain.voice.web.dto.VoiceRequestDTO;
 import fairytale.tbd.domain.voice.web.dto.VoiceResponseDTO;
+import fairytale.tbd.global.annotation.LoginUser;
 import fairytale.tbd.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,12 +27,11 @@ public class VoiceRestController {
 	private final VoiceCommandService voiceCommandService;
 
 	@PostMapping("")
-	public ApiResponse<VoiceResponseDTO.AddVoiceResultDTO> addVoice(@Valid @ModelAttribute VoiceRequestDTO.AddVoiceDTO request){
-		// TODO 이미 존재하는 Voice 인지 검증
+	public ApiResponse<VoiceResponseDTO.AddVoiceResultDTO> addVoice(
+		@Valid @ModelAttribute VoiceRequestDTO.AddVoiceDTO request, @LoginUser User user) {
 		LOGGER.info("request = {}", request);
-		Voice voice = voiceCommandService.uploadVoice(request);
+		Voice voice = voiceCommandService.uploadVoice(request, user);
 		return ApiResponse.onSuccess(VoiceConverter.toAddVoiceResult(voice));
 	}
-
 
 }
