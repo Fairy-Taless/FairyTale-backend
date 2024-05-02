1f5343b6868eaf110bbc30233a6b70f69667de36
junhaa <2171326@hansung.ac.kr>
2024-05-02 09:00:45 +0900

    :sparkles: FEAT. 음성 변환 서비스 로직 추가

--- src/main/java/fairytale/tbd/domain/voice/service/VoiceCommandService.java
+++ src/main/java/fairytale/tbd/domain/voice/service/VoiceCommandService.java
@@ -3,7 +3,10 @@ package fairytale.tbd.domain.voice.service;
 import fairytale.tbd.domain.user.entity.User;
 import fairytale.tbd.domain.voice.entity.Voice;
 import fairytale.tbd.domain.voice.web.dto.VoiceRequestDTO;
+import fairytale.tbd.domain.voice.web.dto.VoiceResponseDTO;
 
 public interface VoiceCommandService {
 	Voice uploadVoice(VoiceRequestDTO.AddVoiceDTO request, User user);
+
+	VoiceResponseDTO.AddTTSSegmentResultDTO addTTSSegment(VoiceRequestDTO.AddSegmentDTO request);
 }
