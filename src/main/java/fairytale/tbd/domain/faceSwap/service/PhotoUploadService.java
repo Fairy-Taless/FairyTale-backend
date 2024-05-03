30ca62abdeb147c6d0a263bcbef0efd655e03077
ghvfgfcht <kjh03124@naver.com>
2024-05-03 22:03:36 +0900

    :sparkles: FEAT. PhotoUploadService savePhotos 메서드 매개변수 user 추가

--- src/main/java/fairytale/tbd/domain/faceSwap/service/PhotoUploadService.java
+++ src/main/java/fairytale/tbd/domain/faceSwap/service/PhotoUploadService.java
@@ -1,11 +1,11 @@
 package fairytale.tbd.domain.faceSwap.service;
 
 import fairytale.tbd.domain.faceSwap.web.dto.FaceDetectRequestDto;
-import fairytale.tbd.domain.faceSwap.web.dto.PhotoUploadRequestDto;
+import fairytale.tbd.domain.user.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface PhotoUploadService {
-    FaceDetectRequestDto savePhotos(MultipartFile photoUploadRequestDto) throws IOException;
+    FaceDetectRequestDto savePhotos(User userId, MultipartFile photoUploadRequestDto) throws IOException;
 }
