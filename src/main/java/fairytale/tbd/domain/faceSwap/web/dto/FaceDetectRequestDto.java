294019a876d2e728f476401f26c0122b5cb1efc7
ghvfgfcht <kjh03124@naver.com>
2024-05-03 21:53:58 +0900

    :art: ADD. @ToString 어노테이션 추가

--- src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceDetectRequestDto.java
+++ src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceDetectRequestDto.java
@@ -2,9 +2,11 @@ package fairytale.tbd.domain.faceSwap.web.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class FaceDetectRequestDto {
 
     private String imgURL;
