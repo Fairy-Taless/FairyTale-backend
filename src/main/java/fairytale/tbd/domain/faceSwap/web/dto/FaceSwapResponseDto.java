e91a1bfb516da339b8af879c8c477595e7af74ba
ghvfgfcht <kjh03124@naver.com>
2024-05-03 21:59:43 +0900

    :sparkles: FEAT. FaceSwapResponseDto 클래스 추가

--- src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceSwapResponseDto.java
+++ src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceSwapResponseDto.java
@@ -1,4 +1,11 @@
 package fairytale.tbd.domain.faceSwap.web.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class FaceSwapResponseDto {
+    private Long fairytaleID;
+
 }
