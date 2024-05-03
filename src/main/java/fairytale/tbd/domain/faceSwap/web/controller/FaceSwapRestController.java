9ca17241233be251d28a3ca430e4321820787981
ghvfgfcht <kjh03124@naver.com>
2024-05-03 16:13:19 +0900

    CHORE. FaceSwapRestController uploadImg method 매개변수 이름 변경

--- src/main/java/fairytale/tbd/domain/faceSwap/web/controller/FaceSwapRestController.java
+++ src/main/java/fairytale/tbd/domain/faceSwap/web/controller/FaceSwapRestController.java
@@ -1,9 +1,14 @@
 package fairytale.tbd.domain.faceSwap.web.controller;
 
+import fairytale.tbd.domain.faceSwap.service.FaceDetectApiServiceImpl;
+import fairytale.tbd.domain.faceSwap.service.FaceSwapApiServiceImpl;
 import fairytale.tbd.domain.faceSwap.service.PhotoUploadServiceImpl;
 import fairytale.tbd.domain.faceSwap.web.dto.FaceDetectRequestDto;
-import fairytale.tbd.domain.faceSwap.web.dto.PhotoUploadRequestDto;
+import fairytale.tbd.domain.faceSwap.web.dto.FaceDetectResponseDto;
+import fairytale.tbd.domain.faceSwap.web.dto.FaceSwapRequestDto;
+import fairytale.tbd.domain.faceSwap.web.dto.FaceSwapResponseDto;
 import fairytale.tbd.domain.user.entity.User;
+import fairytale.tbd.global.annotation.LoginUser;
 import fairytale.tbd.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +24,20 @@ import java.io.IOException;
 public class FaceSwapRestController {
 
     private final PhotoUploadServiceImpl photoUploadService;
+    private final FaceDetectApiServiceImpl faceDetectApiService;
+    private final FaceSwapApiServiceImpl faceSwapApiService;
 
     @PostMapping("/uploadImg")
-    public ApiResponse<FaceDetectRequestDto> uploadImg(/*@LoginUser User user,*/ @ModelAttribute MultipartFile photoUploadRequestDto) throws IOException{
-        FaceDetectRequestDto faceDetectRequestDto = photoUploadService.savePhotos(photoUploadRequestDto);
+    public ApiResponse<FaceDetectRequestDto> uploadImg(@LoginUser User user, @ModelAttribute MultipartFile file) throws IOException{
+        FaceDetectRequestDto faceDetectRequestDto = photoUploadService.savePhotos(user, file);
         log.info("Face detect request : {}", faceDetectRequestDto);
         return ApiResponse.onSuccess(faceDetectRequestDto);
     }
+
+/*    @GetMapping("/createCharacter")
+    public ApiResponse<FaceSwapResponseDtoDto> detectFace(FaceDetectRequestDto faceDetectRequestDto) throws IOException {
+        FaceDetectResponseDto faceDetectResponseDto = faceDetectApiService.getOptFromFaceDetectApi(faceDetectRequestDto);
+
+        return ApiResponse.onSuccess();
+    }*/
 }
