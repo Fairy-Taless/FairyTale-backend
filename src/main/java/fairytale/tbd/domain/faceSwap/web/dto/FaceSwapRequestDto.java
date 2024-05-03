de298acf12ccae95ce6611032f042d80378a8d98
ghvfgfcht <kjh03124@naver.com>
2024-05-03 21:59:00 +0900

    :sparkles: FEAT. FaceSwapRequestDto 클래스 추가

--- src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceSwapRequestDto.java
+++ src/main/java/fairytale/tbd/domain/faceSwap/web/dto/FaceSwapRequestDto.java
@@ -1,4 +1,54 @@
 package fairytale.tbd.domain.faceSwap.web.dto;
 
+import lombok.*;
+
 public class FaceSwapRequestDto {
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FaceSwapRequest {
+
+        private SourceImage sourseImage;
+
+        private TargetImage targetImage;
+
+        // 1 means open, 0 means close
+        private int faceEnhance;
+
+        private String modifyImage;
+
+        private String webHookUrl;
+
+    }
+
+    // Original Image
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SourceImage {
+
+        private String sourcePath;
+
+        private String sourceOpts;
+
+    }
+
+    // User Image
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TargetImage {
+
+        private String targetPath;
+
+        private String targetOpts;
+
+    }
 }
