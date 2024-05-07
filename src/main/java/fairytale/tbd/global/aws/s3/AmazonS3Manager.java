6a1cbda85ad384641c98f6858afc5ef77e0839eb
junhaa <2171326@hansung.ac.kr>
2024-05-08 06:53:34 +0900

    :sparkles: FEAT. S3 InputStream 저장 기능 구현

--- src/main/java/fairytale/tbd/global/aws/s3/AmazonS3Manager.java
+++ src/main/java/fairytale/tbd/global/aws/s3/AmazonS3Manager.java
@@ -1,6 +1,8 @@
 package fairytale.tbd.global.aws.s3;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,8 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
 import fairytale.tbd.global.config.AmazonConfig;
+import fairytale.tbd.global.enums.statuscode.ErrorStatus;
+import fairytale.tbd.global.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,6 +37,12 @@ public class AmazonS3Manager {
 	@Value("${cloud.aws.s3.path.user-voice}")
 	public String TTS_USER_VOICE_PATH;
 
+	@Value("${cloud.aws.s3.path.face-sample}")
+	public String FACE_SWAP_SAMPLE_PATH;
+
+	@Value("${cloud.aws.s3.path.user-face}")
+	public String FACE_SWAP_USER_PATH;
+
 	public String uploadFile(String keyName, MultipartFile file) {
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentType(file.getContentType());
@@ -43,8 +53,28 @@ public class AmazonS3Manager {
 			log.info("result={}", putObjectResult.getContentMd5());
 		} catch (IOException e) {
 			log.error("error at AmazonS3Manager uploadFile : {}", (Object)e.getStackTrace());
+			throw new GeneralException(ErrorStatus._S3_FILE_UPLOAD_ERROR);
 		}
 
+		return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+	}
+
+	public String uploadJpegImageFromUrl(String keyName, String imageUrl) {
+
+		try {
+			URL url = new URL(imageUrl);
+			InputStream inputStream = url.openStream();
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentType("image/jpeg"); // Change content type based on your image type
+			metadata.setContentLength(inputStream.available());
+
+			PutObjectResult putObjectResult = amazonS3.putObject(
+				new PutObjectRequest(amazonConfig.getBucket(), keyName, inputStream, metadata));
+			log.info("result={}", putObjectResult.getContentMd5());
+		} catch (IOException e) {
+			log.error("error at AmazonS3Manager uploadFile : {}", (Object)e.getStackTrace());
+			throw new GeneralException(ErrorStatus._S3_FILE_UPLOAD_ERROR);
+		}
 		return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
 	}
 
