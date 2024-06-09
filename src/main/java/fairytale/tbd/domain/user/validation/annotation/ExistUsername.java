5f08aa4efdc417ee9b8dfd009fbd843be5d063e8
junhaa <2171326@hansung.ac.kr>
2024-06-09 21:29:34 +0900

    :construction_worker: CI. ci 실행

--- src/main/java/fairytale/tbd/domain/user/validation/annotation/ExistUsername.java
+++ src/main/java/fairytale/tbd/domain/user/validation/annotation/ExistUsername.java
@@ -15,9 +15,7 @@ import jakarta.validation.Payload;
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExistUsername {
-	String message() default("이미 존재하는 닉네임입니다.");
-
-	Class<?>[] groups() default {};
+	String message() default ("이미 존재하는 닉네임입니다.");
 
 	Class<? extends Payload>[] payload() default {};
 }
